f2f2a658ec87b3d6a8e9eb89644f076325c2d017
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-11-04 21:49:00 -0800

    Improve browser logs

--- subduction_wasm/src/lib.rs
+++ subduction_wasm/src/lib.rs
@@ -52,9 +52,8 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
-    let config = WasmLayerConfig::new()
-        .with_max_level(tracing::Level::TRACE)
-        .with_colorless_logs();
+    let mut config = WasmLayerConfig::new().with_max_level(tracing::Level::TRACE);
+    config.use_console_methods = true;
 
     wasm_tracing::set_as_global_default_with_config(config).expect("unable to set global default");
 }
