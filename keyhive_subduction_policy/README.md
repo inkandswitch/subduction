2c2ee663dc60a4eec6ae4955aec7faf2a4eb7630
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-01-22 13:27:10 -0800

    create keyhive policy package

---
+++ keyhive_subduction_policy/README.md
@@ -0,0 +1,3 @@
+# keyhive_subduction_policy
+
+Connection policy for Subduction based on Keyhive authorization.
---
+++ keyhive_subduction_policy/src/lib.rs
@@ -0,0 +1,9 @@
+//! Connection policy for Subduction based on Keyhive authorization.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+#[cfg(feature = "std")]
+extern crate std;
+
+// FIXME extern crate alloc;
