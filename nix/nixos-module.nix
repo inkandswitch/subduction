702c759e3dae8fba53614ff243182b4fff25cc44
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-01-22 00:43:25 -0800

    Fix nix module metrics (#16)

--- nix/nixos-module.nix
+++ nix/nixos-module.nix
@@ -129,11 +129,8 @@ in {
                 (toString cfg.server.dataDir)
                 "--timeout"
                 (toString cfg.server.timeout)
-                "--metrics"
-                (lib.boolToString cfg.server.enableMetrics)
-                "--metrics-port"
-                (toString cfg.server.metricsPort)
               ]
+              ++ lib.optionals cfg.server.enableMetrics ["--metrics" "--metrics-port" (toString cfg.server.metricsPort)]
               ++ lib.optionals (cfg.server.peerId != null) ["--peer-id" cfg.server.peerId]
               ++ lib.concatMap (peer: ["--peer" peer]) cfg.server.peers;
           in
