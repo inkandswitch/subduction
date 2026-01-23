40061e1d3d5f4f48646d58a86ca5ebdd70bc8f44
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-01-22 19:01:38 -0800

    Split out package

--- keyhive_subduction_policy/README.md
+++
@@ -1,3 +0,0 @@
-# keyhive_subduction_policy
-
-Connection policy for Subduction based on Keyhive authorization.
--- keyhive_subduction_policy/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Connection policy for Subduction based on Keyhive authorization.
-
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
-
-#[cfg(feature = "std")]
-extern crate std;
-
-// FIXME extern crate alloc;
