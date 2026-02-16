6dcbbeeb8289ffece8fccbafc6d6b8fe4e147cc9
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-02-16 13:44:21 -0800

    Fomrtater

--- subduction_core/benches/subduction.rs
+++ subduction_core/benches/subduction.rs
@@ -29,7 +29,7 @@ mod generators {
     use futures::executor::block_on;
     use std::collections::BTreeSet;
 
-    use rand::{rngs::StdRng, Rng, SeedableRng};
+    use rand::{Rng, SeedableRng, rngs::StdRng};
     use sedimentree_core::{
         blob::{Blob, BlobMeta},
         crypto::{digest::Digest, fingerprint::FingerprintSeed},
@@ -223,7 +223,7 @@ mod generators {
 }
 
 mod id {
-    use criterion::{black_box, BatchSize, BenchmarkId, Criterion, Throughput};
+    use criterion::{BatchSize, BenchmarkId, Criterion, Throughput, black_box};
     use sedimentree_core::id::SedimentreeId;
     use subduction_core::{
         connection::{id::ConnectionId, message::RequestId},
@@ -359,7 +359,7 @@ mod id {
 }
 
 mod message {
-    use criterion::{black_box, BenchmarkId, Criterion, Throughput};
+    use criterion::{BenchmarkId, Criterion, Throughput, black_box};
     use subduction_core::connection::message::Message;
 
     use super::generators::{
@@ -504,7 +504,7 @@ mod message {
 mod sync {
     use std::collections::BTreeSet;
 
-    use criterion::{black_box, BatchSize, BenchmarkId, Criterion, Throughput};
+    use criterion::{BatchSize, BenchmarkId, Criterion, Throughput, black_box};
     use sedimentree_core::{crypto::fingerprint::FingerprintSeed, sedimentree::FingerprintSummary};
     use subduction_core::connection::message::{
         BatchSyncRequest, BatchSyncResponse, Message, SyncResult,
@@ -657,7 +657,7 @@ mod sync {
 mod collections {
     use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 
-    use criterion::{black_box, BenchmarkId, Criterion, Throughput};
+    use criterion::{BenchmarkId, Criterion, Throughput, black_box};
     use sedimentree_core::id::SedimentreeId;
     use subduction_core::{connection::id::ConnectionId, peer::id::PeerId};
 
@@ -842,7 +842,7 @@ mod collections {
 }
 
 mod cloning {
-    use criterion::{black_box, BenchmarkId, Criterion, Throughput};
+    use criterion::{BenchmarkId, Criterion, Throughput, black_box};
     use subduction_core::connection::{id::ConnectionId, message::Message};
 
     use super::generators::{
@@ -946,7 +946,7 @@ mod cloning {
 }
 
 mod display {
-    use criterion::{black_box, Criterion};
+    use criterion::{Criterion, black_box};
     use subduction_core::storage::id::StorageId;
 
     use super::generators::{digest_from_seed, peer_id_from_seed, sedimentree_id_from_seed};
